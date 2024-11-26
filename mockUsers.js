de5cb5eb611fb1ababfd7883788fe5e93509b67f
blaczeus029 <anyachidiebere029@gmail.com>
2024-11-26 11:11:13 +0100

    Update mockUsers with realistic email addresses and added 'username' property

---
+++ mockUsers.js
@@ -0,0 +1,10 @@
+const mockUsers = [
+  { email: "anyachidiebere029@gmail.com", username: "Blaczeus", password: "chidi029" },
+  { email: "james.smith@gmail.com", username: "JamesSmith", password: "password123" },
+  { email: "sophia.johnson@yahoo.com", username: "SophiaJ", password: "securePass456" },
+  { email: "michael.brown@outlook.com", username: "MikeBrown", password: "brownie789" },
+  { email: "olivia.davis@gmail.com", username: "OliviaD", password: "davisSecure101" },
+  { email: "emma.jones@hotmail.com", username: "EmmaJones", password: "emmaRocks202" },
+];
+
+export default mockUsers;
